afe883cc669906724041f9b56ecfff01b5eb30ef
Anthodido <anthony.didomenico678@gmail.com>
2026-05-04 11:16:23 +0200

    P1_T1

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+language = "python"
+version = 3
+Pi_value = 3.14
+Computation_valid = True
+
+print (f"language {language}")
+print (f"version: {version}")
+print (f"Pi approx: {Pi_value:.2f}")
+print (f"Computation valid: {Computation_valid}")
