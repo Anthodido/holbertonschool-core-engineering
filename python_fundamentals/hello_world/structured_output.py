7e6a05b789af3ff38bcfd4366f280e7d7b8218c7
Anthodido <anthony.didomenico678@gmail.com>
2026-05-04 11:50:06 +0200

    bug

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -5,7 +5,7 @@ version = 3
 Pi_value = 3.14159265
 Computation_valid = 4 > 3
 
-print (f"language {language}")
-print (f"version: {version}")
-print (f"Pi approx: {Pi_value:.2f}")
-print (f"Computation valid: {Computation_valid}")
+print(f"language {language}")
+print(f"version: {version}")
+print(f"Pi approx: {Pi_value:.2f}")
+print(f"Computation valid: {Computation_valid}")
