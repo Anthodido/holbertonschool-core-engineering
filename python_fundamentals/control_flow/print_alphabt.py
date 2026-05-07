5ff6ebaa23583d589ebd2c2a22a12775ae18485c
Anthodido <anthony.didomenico678@gmail.com>
2026-05-07 09:51:53 +0200

    modif

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -3,6 +3,6 @@
 result = ""
 for alpha_letters in range(ord('a'), ord('z')+1):
     if alpha_letters == ord('e') or alpha_letters == ord('q'):
-       continue
+      continue
     result = result + chr(alpha_letters)
 print(result)
