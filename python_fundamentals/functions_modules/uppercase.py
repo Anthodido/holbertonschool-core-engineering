24fac04a5426965da7431a75d8b43a1d4832218c
Anthodido <anthony.didomenico678@gmail.com>
2026-05-07 11:23:27 +0200

    t1

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -2,7 +2,7 @@
 
 def uppercase(str):
     for index in str:
-        if ord(index) >= ord('a') and ord(index) <= ord('z'):
-            index = chr(ord(index) - (ord('a') - ord('A')))
-        print(index, end="")
+        if ord(index) >= ord('a') ord(index) <= ord('z'):
+            index = chr(ord(index) - 32)
+        print("{}".format(index), end="")
     print()
