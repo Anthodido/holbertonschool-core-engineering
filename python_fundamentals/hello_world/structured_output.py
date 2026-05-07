0c07c75c5480dad733c7d2a01ef9b9a32479be5c
Anthodido <anthony.didomenico678@gmail.com>
2026-05-07 09:31:54 +0200

    modif

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-language = "Python"
-version = 3
+Language = "Python"
+Version = 3
 Pi_value = 3.14159265
 Computation_valid = 4 > 3
 
 print(f"Language: {language}")
-print(f"version: {version}")
+print(f"Version: {version}")
 print(f"Pi approx: {Pi_value:.2f}")
 print(f"Computation valid: {Computation_valid}")
