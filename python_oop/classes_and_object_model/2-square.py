67abb1302d22ce143b2282e58330750b6f59b47c
Anthodido <anthony.didomenico678@gmail.com>
2026-05-11 14:19:27 +0200

    P1-T2

--- python_oop/classes_and_object_model/2-square.py
+++ python_oop/classes_and_object_model/2-square.py
@@ -4,7 +4,7 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-    if not isinstance(self.__size, int):
-        raise TypeError("size must be an integer")
-    elif self.__size < 0:
-        raise ValueError("size must be >= 0")
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
