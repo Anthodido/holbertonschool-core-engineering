43be868ce7162c72ccfc94fa7f5f0404b0098a3d
Anthodido <anthony.didomenico678@gmail.com>
2026-05-11 14:16:24 +0200

    P1-T2

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
+
+    if not isinstance(self.__size, int):
+        raise TypeError("size must be an integer")
+    elif self.__size < 0:
+        raise ValueError("size must be >= 0")
