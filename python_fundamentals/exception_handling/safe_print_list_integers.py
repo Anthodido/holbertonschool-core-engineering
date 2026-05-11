7ed264302fcd20092cef2dff84bf5d59c39c9bc5
Anthodido <anthony.didomenico678@gmail.com>
2026-05-11 11:20:35 +0200

    P0_T2

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -6,6 +6,9 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count = count + 1
-        except:
+        except TypeError:
             pass
+        except ValueError:
+            pass
+    print()
     return count
