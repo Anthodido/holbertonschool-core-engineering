c1b99d488f7be284ad7bc354ca89f669ecdd8690
Anthodido <anthony.didomenico678@gmail.com>
2026-05-15 10:03:12 +0200

    animal

---
+++ python_oop/abc/animals.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Defines an abstract base class for animals."""
+
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    
+    """Defines an abstract base class for animals."""
+
+    @abstractmethod
+    def sound(self):
+        """Abstract method that defines the sound behavior."""
+        pass
+
+class Dog(Animal):
+
+    """Represents a dog that inherits from the Animal class."""
+
+    def sound(self):
+        """Implements the sound method for a dog."""
+        return "Bark"
+
+class Cat(Animal):
+
+    """Represents a cat that inherits from the Animal class."""
+
+    def sound(self):
+        """Implements the sound method for a cat."""
+        return "Meow"
