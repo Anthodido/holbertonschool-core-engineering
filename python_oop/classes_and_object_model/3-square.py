78fc78ec78270204ac203c7dba91819d731d8d56
Anthodido <anthony.didomenico678@gmail.com>
2026-05-15 09:08:56 +0200

    3square

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -7,13 +7,14 @@ class Square:
 
     """Represents a square."""
 
-    def __init__(self, size):
-        self.__size = size
+    def __init__(self, size=0):
 
-        if not isinstance(self.__size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif self.__size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+
+        self.__size = size
 
     def area(self):
         return self.__size ** 2
