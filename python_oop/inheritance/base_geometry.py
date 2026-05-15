58c499fb06dac9857e251a9f24d338a652cbdbe7
Anthodido <anthony.didomenico678@gmail.com>
2026-05-15 09:17:25 +0200

    geométrie

--- python_oop/inheritance/base_geometry.py
+++ python_oop/inheritance/base_geometry.py
@@ -9,7 +9,7 @@ class BaseGeometry:
 
     def area(self):
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         if not type(value) is int:
             raise TypeError(f"{name} must be an integer")
