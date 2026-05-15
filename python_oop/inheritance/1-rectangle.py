67c647e854962741bdfc0306cfee019c1e90f2bc
Anthodido <anthony.didomenico678@gmail.com>
2026-05-15 09:30:29 +0200

    1rectangle

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -3,7 +3,7 @@
 """Defines a Rectangle class that inherits from BaseGeometry."""
 
 
-from base_geometry import BaseGeometry
+BaseGeometry = __import__('base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
 
