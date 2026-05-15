de104a384b919a5d7bbfee30a528a1252a2970a5
Anthodido <anthony.didomenico678@gmail.com>
2026-05-15 09:02:39 +0200

    6square

--- python_oop/classes_and_object_model/6-square.py
+++ python_oop/classes_and_object_model/6-square.py
@@ -13,7 +13,7 @@ class Square:
 
     def area(self):
         return self.__size ** 2
-    
+
     @property
     def size(self):
         return self.__size
@@ -21,7 +21,8 @@ class Square:
     @property
     def position(self):
         return self.__position
-#______________setter________________
+
+    # setter
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -29,17 +30,16 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or\
-        not all(isinstance(num, int) for num in value) or not all(num >= 0 for num in value):
+        if (not isinstance(value, tuple) or len(value) != 2
+                or not all(isinstance(num, int) for num in value)
+                or not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-
-
-#______________my_print________________
+    # my_print
     def my_print(self):
         if self.__size == 0:
             print()
@@ -48,7 +48,6 @@ class Square:
             for i in range(self.__size):
                 print(" " * self.__position[0], end="")
                 print("#" * self.__size)
-
 
     def __str__(self):
         if self.__size == 0:
