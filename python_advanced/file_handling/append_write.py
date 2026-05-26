9d98f00092a7a97f723a38518d4a7d62727ee6e3
Anthodido <anthony.didomenico678@gmail.com>
2026-05-26 10:55:17 +0200

    append_write.py

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -5,7 +5,7 @@
 
 def append_write(filename="", text=""):
 
-    """Appends a string to a text file (UTF8) and returns the number of characters added."""
+    """Appends a string and returns the number of characters added."""
 
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)    
