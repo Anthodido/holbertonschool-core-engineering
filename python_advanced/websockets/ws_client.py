4c20c93ba4542a0b33be6b8f3ab5f69b1fe8a0bf
Anthodido <anthony.didomenico678@gmail.com>
2026-07-16 11:06:31 +0200

    erreur

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -4,23 +4,11 @@ import asyncio
 import websockets
 
 
-async def connect_and_send(uri: str, text: str, deadline: float = 15.0, delay: float = 0.3) -> str:
-    """Send text over uri and return the single response received.
-
-    Retries the initial connection to tolerate a server that has not
-    finished starting up yet.
-    """
-    loop = asyncio.get_event_loop()
-    start = loop.time()
-    while True:
-        try:
-            async with websockets.connect(uri) as websocket:
-                await websocket.send(text)
-                return await websocket.recv()
-        except OSError:
-            if loop.time() - start >= deadline:
-                raise
-            await asyncio.sleep(delay)
+async def connect_and_send(uri: str, text: str) -> str:
+    """Send text over uri and return the single response received."""
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(text)
+        return await websocket.recv()
 
 
 async def main():
